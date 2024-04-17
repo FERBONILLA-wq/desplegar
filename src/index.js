1d3ee965f8ebddc52fa5fa9ae0af6780fb07a999
FERBONILLA-wq <84981941+FERBONILLA-wq@users.noreply.github.com>
2024-04-16 22:57:43 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-import app from './app.js';
-import {connectDB} from './config/db.js';
 
-const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(3000, () => {
-      console.log("Servidor arriba en el puerto 3000");
-    });
-  } catch (error) {
-    console.error("Error al iniciar el servidor:", error);
-  }
-};
 
-startServer();
+import app from "app.js";
+import { connectDB } from "./config/db.js";
 
+connectDB();
+app.listen(3000, () => {
+  console.log("Up");
+});
